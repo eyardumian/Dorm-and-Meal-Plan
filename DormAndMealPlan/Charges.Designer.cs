edc65b8cc439531e8df98dc4520017ef23f66a19
Erika Yardumian <eyardumian@hotmail.com>
2019-09-16 13:42:42 -0700

    Designed charges form

--- DormAndMealPlan/Charges.Designer.cs
+++ DormAndMealPlan/Charges.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTotalCost = new System.Windows.Forms.Label();
+            this.labelCharges = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTotalCost
+            // 
+            this.labelTotalCost.AutoSize = true;
+            this.labelTotalCost.Location = new System.Drawing.Point(283, 58);
+            this.labelTotalCost.Name = "labelTotalCost";
+            this.labelTotalCost.Size = new System.Drawing.Size(215, 13);
+            this.labelTotalCost.TabIndex = 0;
+            this.labelTotalCost.Text = "The total cost of your dorm and meal plan is:";
+            this.labelTotalCost.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelCharges
+            // 
+            this.labelCharges.AutoSize = true;
+            this.labelCharges.Location = new System.Drawing.Point(369, 111);
+            this.labelCharges.Name = "labelCharges";
+            this.labelCharges.Size = new System.Drawing.Size(0, 13);
+            this.labelCharges.TabIndex = 1;
+            // 
+            // Charges
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelCharges);
+            this.Controls.Add(this.labelTotalCost);
+            this.Name = "Charges";
+            this.ShowIcon = false;
             this.Text = "Charges";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTotalCost;
+        private System.Windows.Forms.Label labelCharges;
     }
 }
--- DormAndMealPlan/Charges.cs
+++ DormAndMealPlan/Charges.cs
@@ -16,5 +16,10 @@ namespace DormAndMealPlan
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
