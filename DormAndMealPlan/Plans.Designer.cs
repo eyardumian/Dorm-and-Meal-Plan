c271d163a20dfb5084051bb453d31095c7554730
Erika Yardumian <eyardumian@hotmail.com>
2019-09-16 13:37:27 -0700

    Added meal plans

--- DormAndMealPlan/Plans.Designer.cs
+++ DormAndMealPlan/Plans.Designer.cs
@@ -33,6 +33,11 @@
             this.radioButtonPikeHall = new System.Windows.Forms.RadioButton();
             this.radioButtonFarthingHall = new System.Windows.Forms.RadioButton();
             this.radioButtonUniversitySuites = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton7Meals = new System.Windows.Forms.RadioButton();
+            this.radioButton14Meals = new System.Windows.Forms.RadioButton();
+            this.radioButtonUnlimitedMeals = new System.Windows.Forms.RadioButton();
+            this.buttonSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // radioButtonAllenHall
@@ -88,11 +93,68 @@
             this.radioButtonUniversitySuites.Text = "University Suites $2,500 per semester";
             this.radioButtonUniversitySuites.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Select a Meal Plan:";
+            // 
+            // radioButton7Meals
+            // 
+            this.radioButton7Meals.AutoSize = true;
+            this.radioButton7Meals.Location = new System.Drawing.Point(424, 59);
+            this.radioButton7Meals.Name = "radioButton7Meals";
+            this.radioButton7Meals.Size = new System.Drawing.Size(198, 17);
+            this.radioButton7Meals.TabIndex = 6;
+            this.radioButton7Meals.TabStop = true;
+            this.radioButton7Meals.Text = "7 meals per week $600 per semester";
+            this.radioButton7Meals.UseVisualStyleBackColor = true;
+            // 
+            // radioButton14Meals
+            // 
+            this.radioButton14Meals.AutoSize = true;
+            this.radioButton14Meals.Location = new System.Drawing.Point(424, 93);
+            this.radioButton14Meals.Name = "radioButton14Meals";
+            this.radioButton14Meals.Size = new System.Drawing.Size(224, 18);
+            this.radioButton14Meals.TabIndex = 7;
+            this.radioButton14Meals.TabStop = true;
+            this.radioButton14Meals.Text = "14 meals per week $1,200 per semester";
+            this.radioButton14Meals.UseCompatibleTextRendering = true;
+            this.radioButton14Meals.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonUnlimitedMeals
+            // 
+            this.radioButtonUnlimitedMeals.AutoSize = true;
+            this.radioButtonUnlimitedMeals.Location = new System.Drawing.Point(424, 128);
+            this.radioButtonUnlimitedMeals.Name = "radioButtonUnlimitedMeals";
+            this.radioButtonUnlimitedMeals.Size = new System.Drawing.Size(197, 17);
+            this.radioButtonUnlimitedMeals.TabIndex = 8;
+            this.radioButtonUnlimitedMeals.TabStop = true;
+            this.radioButtonUnlimitedMeals.Text = "Unlimited meals $1,700 per semester";
+            this.radioButtonUnlimitedMeals.UseVisualStyleBackColor = true;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(322, 217);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 9;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            // 
             // Plans
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.radioButtonUnlimitedMeals);
+            this.Controls.Add(this.radioButton14Meals);
+            this.Controls.Add(this.radioButton7Meals);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.radioButtonUniversitySuites);
             this.Controls.Add(this.radioButtonFarthingHall);
             this.Controls.Add(this.radioButtonPikeHall);
@@ -106,12 +168,16 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.RadioButton radioButtonAllenHall;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RadioButton radioButtonPikeHall;
-        private System.Windows.Forms.RadioButton radioButtonFarthingHall;
-        private System.Windows.Forms.RadioButton radioButtonUniversitySuites;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSubmit;
+        public System.Windows.Forms.RadioButton radioButtonAllenHall;
+        public System.Windows.Forms.RadioButton radioButtonPikeHall;
+        public System.Windows.Forms.RadioButton radioButtonFarthingHall;
+        public System.Windows.Forms.RadioButton radioButtonUniversitySuites;
+        public System.Windows.Forms.RadioButton radioButton7Meals;
+        public System.Windows.Forms.RadioButton radioButton14Meals;
+        public System.Windows.Forms.RadioButton radioButtonUnlimitedMeals;
     }
 }
 
